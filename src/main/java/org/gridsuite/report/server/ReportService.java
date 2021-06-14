3d200e237bd10eeff5bbc4783e537508bc549fd5
Jon Harper <jon.harper87@gmail.com>
2021-06-14 08:13:46 +0200

    compilation fixes
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>

--- src/main/java/org/gridsuite/report/server/ReportService.java
+++ src/main/java/org/gridsuite/report/server/ReportService.java
@@ -16,9 +16,7 @@ import org.gridsuite.report.server.entities.TreeReportEntity;
 import org.gridsuite.report.server.repositories.ReportRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 import java.util.stream.Collectors;
