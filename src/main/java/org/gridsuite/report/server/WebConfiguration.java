c3549e1904ead21c2010f99167ff3019c5673dc2
jacqbors <jacques.borsenberger@rte-france.com>
2021-06-28 13:05:24 +0200

    dont mix mvc/webflux
    Signed-off-by: jacqbors <jacques.borsenberger@rte-france.com>

--- src/main/java/org/gridsuite/report/server/WebConfiguration.java
+++ src/main/java/org/gridsuite/report/server/WebConfiguration.java
@@ -6,21 +6,11 @@ import com.powsybl.commons.reporter.ReporterModelDeserializer;
 import com.powsybl.commons.reporter.ReporterModelJsonModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebFluxConfig implements WebFluxConfigurer {
-
-    @Override
-    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
-        var objectMapper = objectMapper();
-        configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
-        configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
-    }
+public class WebConfiguration implements WebMvcConfigurer {
 
     public static ObjectMapper createObjectMapper() {
         var objectMapper = Jackson2ObjectMapperBuilder.json().build();
