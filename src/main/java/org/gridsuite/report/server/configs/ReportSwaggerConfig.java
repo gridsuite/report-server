dad4902fcf463374ffb7128109f0e3950c493a2d
Franck LECUYER <franck.lecuyer@rte-france.com>
2021-08-02 12:59:40 +0200

    Replace springfox with springdoc
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/report/server/configs/ReportSwaggerConfig.java
+++ src/main/java/org/gridsuite/report/server/configs/ReportSwaggerConfig.java
@@ -6,47 +6,23 @@
  */
 package org.gridsuite.report.server.configs;
 
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
-import org.gridsuite.report.server.ReportController;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.gridsuite.report.server.ReportApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Jacques Borsenberger <jacques.borsenberger at rte-france.com>
  */
 @Configuration
-@EnableSwagger2
 public class ReportSwaggerConfig {
     @Bean
-    public Docket produceApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(ReportController.class.getPackage().getName()))
-                .paths(paths())
-                .build();
-    }
-
-    // Describe your apis
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    public OpenAPI createOpenApi() {
+        return new OpenAPI()
+            .info(new Info()
                 .title("Reports API")
                 .description("This is the documentation of the reports REST API")
-                .build();
-    }
-
-    // Only select apis that matches the given Predicates.
-    private Predicate<String> paths() {
-        // Match all paths except /error
-        return Predicates.and(PathSelectors.regex("/" + ".*"),
-                Predicates.not(PathSelectors.regex("/error.*")));
+                .version(ReportApi.API_VERSION));
     }
 }
