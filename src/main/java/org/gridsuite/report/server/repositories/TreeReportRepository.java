2a245b1317c8ed7f671ae289ffacaa5b861afa83
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2023-10-24 22:02:34 +0200

    True SQL CTE

--- src/main/java/org/gridsuite/report/server/repositories/TreeReportRepository.java
+++ src/main/java/org/gridsuite/report/server/repositories/TreeReportRepository.java
@@ -36,36 +36,23 @@ public interface TreeReportRepository extends JpaRepository<TreeReportEntity, UU
     /* TODO to remove when upgrade to new spring-data-jpa, use deleteAllByIdInBatch */
     void deleteAllByIdNodeIn(List<UUID> lst);
 
-    @Query(value = "WITH RECURSIVE fulltree(idNode) AS ("
-        + "SELECT t.id_node FROM tree_report t WHERE t.parent_report = ?1 "
-        + "UNION ALL( "
-        + "SELECT t.id_node "
-        + "FROM tree_report t, fulltree ft "
-        + "WHERE t.parent_report = ft.idNode)) "
-        + "SELECT cast(idNode as varchar) FROM fulltree "
-        + "UNION ALL( "
-        + "SELECT cast(id_node as varchar) FROM tree_report "
-        + "WHERE id_node = ?1)", nativeQuery = true)
+    @Query(value = "WITH RECURSIVE cte AS ("
+        + "SELECT t.id_node FROM tree_report t WHERE t.id_node = ?1 "
+        + "UNION "
+        + "SELECT t.id_node FROM tree_report t, cte ft WHERE t.parent_report = ft.id_node"
+        + ") SELECT id_node FROM cte", nativeQuery = true)
     //TODO we should be able to get hibernate to do this projection..
-    //TODO we cast to varchar otherwise we get
-    //     org.hibernate.MappingException: No Dialect mapping for JDBC type: 1111
-    //     To be revisited when https://github.com/spring-projects/spring-data-jpa/issues/1796
-    //     is fixed.
     List<String> findAllTreeReportIdsRecursivelyByParentTreeReport(UUID parentId);
 
-    /* get all treeReports id, from the given root to the last leaf sub report */
-    @Query(value = "WITH RECURSIVE get_nodes(idNode) AS ("
+    /**
+     * get all treeReports id, from the given root to the last leaf subreport
+     */
+    @Query(value = "WITH RECURSIVE cte AS ("
         + "SELECT t.id_node FROM tree_report t where t.id_node = ?1 "
-        + "UNION ALL("
-        + "SELECT t.id_node "
-        + "FROM tree_report t, get_nodes sg "
-        + "WHERE t.parent_report = sg.idNode)) "
-        + "SELECT cast(idNode as varchar) from get_nodes", nativeQuery = true)
+        + "UNION "
+        + "SELECT t.id_node FROM tree_report t, cte sg WHERE t.parent_report = sg.id_node"
+        + ") SELECT id_node from cte", nativeQuery = true)
     //TODO we should be able to get hibernate to do this projection..
-    //TODO we cast to varchar otherwise we get
-    //     org.hibernate.MappingException: No Dialect mapping for JDBC type: 1111
-    //     To be revisited when https://github.com/spring-projects/spring-data-jpa/issues/1796
-    //     is fixed.
     List<String> getSubReportsNodes(UUID reportId);
 
     List<TreeReportEntity> findAllByReportIdAndName(UUID uuid, String name);
