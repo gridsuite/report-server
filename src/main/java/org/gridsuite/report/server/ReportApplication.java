85f5789aaa85adaa61fadc25c403b8c9784342c2
sBouzols <sylvain.bouzols@gmail.com>
2023-11-22 16:53:57 +0100

    update to gridsuite dependencies 28
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/report/server/ReportApplication.java
+++ src/main/java/org/gridsuite/report/server/ReportApplication.java
@@ -6,7 +6,6 @@
  */
 package org.gridsuite.report.server;
 
-import com.powsybl.ws.commons.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,7 +17,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReportApplication {
 
     public static void main(String[] args) {
-        Utils.initProperties();
         SpringApplication.run(ReportApplication.class, args);
     }
 
