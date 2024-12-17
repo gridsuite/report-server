920462645dedb1a32271d5d495781aeb59e5c9c2
Hugo Marcellin <hugo.marcelin@rte-france.com>
2024-12-17 16:43:22 +0100

    Factorize imports

--- src/main/java/org/gridsuite/report/server/entities/ReportNodeEntity.java
+++ src/main/java/org/gridsuite/report/server/entities/ReportNodeEntity.java
@@ -6,17 +6,7 @@
  */
 package org.gridsuite.report.server.entities;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
